2fb1442b4f3c52dfe1fbdd2b499465e829421542
Sergii Kochetov <sergii.koch@gmail.com>
2023-04-17 19:33:53 +0300

    Init commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import FontAwesomeIcon from '@/assets/icons/fontawesome-icons';
+import VueVideoPlayer from '@videojs-player/vue';
+import { createPinia } from 'pinia';
+import 'video.js/dist/video-js.css';
+import { createApp } from 'vue';
+
+import App from './App.vue';
+import router from './router';
+
+import './assets/styles/main.scss';
+
+const app = createApp(App);
+
+app
+  .use(createPinia())
+  .use(router)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .use(VueVideoPlayer)
+  .mount('#app');
